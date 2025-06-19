fe92d6d38de07f28ab6a82912144dd9f7b0fa3a8
Om Prakash <dr.omprakashmbbs@gmail.com>
2025-06-19 15:14:04 +0000

    Add these following link tags and paste them into the head of your HTML

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,10 @@ export default function RootLayout({
             href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Raleway:wght@400;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
         </head>
         <body className={`${geistSans.variable} ${geistMono.variable} font-body antialiased`}>
           <ThemeProvider
