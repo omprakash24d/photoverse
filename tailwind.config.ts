bd349e03e0f39df056fdbb598ec04dff055f35c0
Om Prakash <dr.omprakashmbbs@gmail.com>
2025-06-19 14:02:19 +0000

    Improve the font i don;t liked it

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Alegreya', 'serif'],
-        headline: ['Belleza', 'sans-serif'],
+        body: ['Lora', 'serif'],
+        headline: ['Raleway', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
